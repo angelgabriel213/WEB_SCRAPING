9ea4b166c3f94720ee707dd9b1556dc6d931f8cb
angel gabriel aguas jimenez <angelaguasj@tecnocomfenalco.edu.co>
2026-09-21 10:08:49 -0500

    refactor: delegate store search to service layer

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from scrapers.mercadolibre import scrape_mercadolibre
 from scrapers.alkosto import scrape_alkosto
 
 from utils.ai_compare import compare_all_products
-from utils.cache import get_cached_products, init_cache, save_products
+from services.search_service import StoreScraper, search_store
 
 
 logging.basicConfig(
@@ -31,29 +31,12 @@ CATALOGO = {
 }
 
 
-def get_products_with_cache(query, store, scraper):
-    """Return fresh cache data or scrape the store when cache is missing."""
-    cached = get_cached_products(query, store)
-
-    if cached:
-        logger.info("%s | CACHE | %s productos", store, len(cached))
-        return cached
-
-    try:
-        products = scraper(query) or []
-        logger.info("%s | SCRAPE | %s productos", store, len(products))
-
-        if products:
-            save_products(query, products)
-
-        return products
-
-    except Exception:
-        logger.exception("%s | ERROR | scraper failed", store)
-        return []
-
-
-@app.route("/")
+def get_def get_products_with_cache(query, store, scraper):
+    """Backward-compatible wrapper around the search service."""
+    return search_store(
+        query,
+        StoreScraper(name=store, scraper=scraper),
+    )oute("/")
 def landing():
     return render_template("home.html")
 
